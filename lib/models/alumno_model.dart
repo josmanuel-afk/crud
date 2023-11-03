f938a02b8181b1768ff8dc682794feafc2539a56
josemanuel-afk <henaomanuel673@gmail.com>
2023-11-02 20:35:13 -0500

    new

--- lib/models/alumno_model.dart
+++ lib/models/alumno_model.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 Alumno noteFromJson(String str) => Alumno.fromJson(json.decode(str));
 
 String noteToJson(Alumno data) => json.encode(data.toJson());
-
 class Alumno {
   int? id;
   String nombre;
